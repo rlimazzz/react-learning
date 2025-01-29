36cb5977eace1260efa091cbb2325fdfb0545be9
Ryan <ryangabryel2005@gmail.com>
2025-01-28 21:31:16 -0300

    Created a card component

---
+++ card-components/src/Card.jsx
@@ -0,0 +1,13 @@
+import './card.css'
+
+function Card() {
+    return(
+       <div className="card">
+            <img src="./camaleao.webp" className="profilePicture"></img>
+            <h2>rlimazzz</h2>
+            <p>I'm learning react</p>
+       </div> 
+    );
+}
+
+export default Card
